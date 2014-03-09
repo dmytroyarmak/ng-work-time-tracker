fe61a18ebf8aa39d0e39d4de0290e12fab230cb8
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-03-09 13:01:34 +0200

    Add 'Choose Date' button

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -1,3 +1,4 @@
 angular.module('workTimeTrackerApp').controller('StatisticsCtrl', ['$scope', 'activities', function ($scope, activities) {
   $scope.activities = activities.getAll();
+  $scope.today = new Date();
 }]);
